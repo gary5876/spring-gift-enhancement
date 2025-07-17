912a0764b61c34aa14d428fcc29308e33da3f9d4
gary5876 <gjw0622@gmail.com>
2025-07-18 05:46:58 +0900

    refactor(member): MemberRepository JPA기반으로 수정

--- src/main/java/gift/member/repository/MemberRepository.java
+++ src/main/java/gift/member/repository/MemberRepository.java
@@ -1,7 +1,6 @@
 package gift.member.repository;
 
 import gift.member.entity.Member;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,4 +8,6 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
